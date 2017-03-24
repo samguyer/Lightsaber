e90db5de5d08fdcf1e5e7c1b4d097a377df96caa
Sam Guyer <sam.guyer@gmail.com>
2017-03-23 21:54:35 -0400

    Recent updates
    Added the boards

--- code/Lightsaber_3.0.ino
+++ code/Lightsaber_3.0.ino
@@ -1,20 +1,31 @@
 #include <adel.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <DFPlayer_Mini_Mp3.h>
+#include "DFRobotDFPlayerMini.h"
 #include <Adafruit_LSM9DS0.h>
 
 // -- Saber state --------------------------
 
 bool g_saber_on = false;
 
-// -- Button -------------------------------
+// -- On/Off Button -------------------------------
 
 #define BUTTON_PIN 4
 
+// -- Mode control
+//    Determines what the rotary encoder does
+
+#define MODE_BUTTON_PIN 8
+#define BRIGHTNESS_A_PIN 5
+#define BRIGHTNESS_B_PIN 6
+
+enum Mode { NO_MODE, BRIGHTNESS_MODE, VOLUME_MODE };
+
+Mode g_Mode;
+
 // -- Blade --------------------------------
 
-#define NUM_SEGMENTS 10
+#define NUM_SEGMENTS 16
 
 // -- PCA9685 driver
 //    Called this way, it uses the default address 0x40
@@ -23,10 +34,15 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define TOTAL_LENGTH 1200
 #define SEGMENT_LENGTH (TOTAL_LENGTH / NUM_SEGMENTS)
 
-#define TIME_TO_ACTIVATE 600
-#define TIME_TO_DEACTIVATE 600
+int g_cur_length = 0;
 
-unsigned int g_max_brightness = 4095;
+#define TIME_TO_ACTIVATE 600
+#define TIME_TO_DEACTIVATE 800
+
+#define MAX_BRIGHTNESS 2400
+#define MIN_BRIGHTNESS 500
+
+int g_cur_brightness = 1400;
 
 // -- Gyroscope ----------------------------
 
@@ -36,41 +52,24 @@ typedef enum { NONE, UP, DOWN, LEFT, RIGHT } Direction;
 
 // -- Sounds -------------------------------
 
-#define SABER_ON_SOUND 1
-#define SABER_OFF_SOUND 2
-#define LONG_HUM_SOUND 3
-#define MEDIUM_HUM_SOUND 4
-#define DOUBLE_HUM_SOUND 5
-#define SHORT_HUM_SOUND 6
-#define SWING_1_SOUND 7
-#define SWING_2_SOUND 8
-#define SWING_3_SOUND 9
+DFRobotDFPlayerMini myDFPlayer;
 
-// -----------------------------------------------
+int g_volume = 18;
+#define MAX_VOLUME 28
+#define MIN_VOLUME 1
 
-// -- Render the saber
-//    Length is a number between 0 and 1000
-void render(int len)
-{
-  int segments_on = len / SEGMENT_LENGTH;
-  int partial = len - segments_on * SEGMENT_LENGTH;
+#define BUSY_BUTTON 3
 
-  for (int i = 0; i < NUM_SEGMENTS; i++) {
-    if (i < segments_on) {
-      pwm.setPWM(i, 0, g_max_brightness );
-      // analogWrite(SEG_PIN[i], 255);
-    }
-    if (i == segments_on) {
-      int level = map(partial/2, 0, SEGMENT_LENGTH, 0, g_max_brightness);
-      pwm.setPWM(i, 0, level );
-      // analogWrite(SEG_PIN[i], dim8_video(partial));
-    }
-    if (i > segments_on) {
-      pwm.setPWM(i, 0, 0 );
-      // analogWrite(SEG_PIN[i], 0);
-    }
-  }
-}
+#define SABER_ON_SOUND 36
+#define SABER_OFF_SOUND 34
+
+#define SWING_MIN_SOUND 49
+#define SWING_MAX_SOUND 64
+
+#define CLASH_MIN_SOUND 17
+#define CLASH_MAX_SOUND 24
+
+// ----------------------------------------------------------------
 
 adel waitbutton(int pin)
 {
@@ -86,21 +85,202 @@ adel waitbutton(int pin)
    aend;
 }
 
-int curlevel = 0;
-long saber_start = 0;
+// -- Render the saber
+//    Length is a number between 0 and TOTAL_LENGTH
+void render()
+{  
+  int segments_on = g_cur_length / SEGMENT_LENGTH;
+  int partial = g_cur_length - segments_on * SEGMENT_LENGTH;
+
+  for (int i = 0; i < NUM_SEGMENTS; i++) {
+    if (i < segments_on) {
+      pwm.setPWM(i, 0, g_cur_brightness );
+    }
+    if (i == segments_on) {
+      int level = map(partial/2, 0, SEGMENT_LENGTH, 0, g_cur_brightness);
+      pwm.setPWM(i, 0, level );
+    }
+    if (i > segments_on) {
+      pwm.setPWM(i, 0, 0 );
+    }
+  }
+}
+
+// ----------------------------------------------------------------
+//  Rotary encoder
+//  Function depends on the mode
+
+adel waitTurn(int pinA, int pinB)
+{
+  abegin:
+  while (1) {
+    await( digitalRead(pinA) == HIGH );
+    await( digitalRead(pinA) == LOW  );
+    if ( digitalRead(pinA) == LOW) {
+      await( digitalRead(pinB) == HIGH );
+      await( digitalRead(pinB) == LOW  );
+      if ( digitalRead(pinB) == LOW && digitalRead(pinA) == LOW)
+        afinish;
+    }
+  }
+  aend;
+}
+
+adel adjustments()
+{
+  int delta = 0;
+  abegin:
+  while (1) {
+    delta = 0;
+    auntil( waitTurn(BRIGHTNESS_A_PIN, BRIGHTNESS_B_PIN), 
+            waitTurn(BRIGHTNESS_B_PIN, BRIGHTNESS_A_PIN) ) {
+      delta = 1;
+    } else {
+      delta = -1;
+    }
+    
+    if (delta != 0) {
+        if (g_Mode == BRIGHTNESS_MODE) {
+          g_cur_brightness += 50 * delta;
+          if (g_cur_brightness > MAX_BRIGHTNESS) g_cur_brightness = MAX_BRIGHTNESS;
+          if (g_cur_brightness < MIN_BRIGHTNESS) g_cur_brightness = MIN_BRIGHTNESS;
+          render();
+        }
+        if (g_Mode == VOLUME_MODE) {
+          g_volume += delta;
+          if (g_volume > MAX_VOLUME) g_volume = MAX_VOLUME;
+          if (g_volume < MIN_VOLUME) g_volume = MIN_VOLUME;
+          myDFPlayer.volume(g_volume);  //Set volume value (0~30)
+        }
+    }
+  }
+  aend;
+}
+
+adel mode()
+{
+  abegin:
+  while (1) {
+    andthen( waitbutton(MODE_BUTTON_PIN) );
+    if (g_Mode == VOLUME_MODE)          g_Mode = NO_MODE;
+    else if (g_Mode == BRIGHTNESS_MODE) g_Mode = VOLUME_MODE;
+    else if (g_Mode == NO_MODE)         g_Mode = BRIGHTNESS_MODE;
+  }
+  aend;
+}
+
+// ----------------------------------------------------------------
+//  Sounds
+//  Based on the motion
+
+Adafruit_LSM9DS0::lsm9ds0Vector_t prev_accelData;
+Adafruit_LSM9DS0::lsm9ds0Vector_t cur_accelData;
+
+float accelDelta()
+{
+  float dx = cur_accelData.x - prev_accelData.x;
+  float dy = cur_accelData.y - prev_accelData.y;
+  float dz = cur_accelData.z - prev_accelData.z;
+  return dx * dx + dy * dy + dz * dz;
+}
+
+bool getClash()
+{
+  bool clash = false;
+  prev_accelData = cur_accelData;
+  cur_accelData.x = lsm.accelData.x / 1000.0;
+  cur_accelData.y = lsm.accelData.y / 1000.0;
+  cur_accelData.z = lsm.accelData.z / 1000.0;
+  float dx = cur_accelData.x - prev_accelData.x;
+  float dy = cur_accelData.y - prev_accelData.y;
+  float dz = cur_accelData.z - prev_accelData.z;
+  float d = dx * dx + dy * dy + dz * dz;
+  // float d = accelDelta();
+  // -- Threshold for clash
+  // Serial.println(d);
+  if (d > 2200.0) clash = true;
+  return clash;
+}
+
+
+Direction getSwing()
+{
+  Direction swing = NONE;
+  
+  float gyroY = ((float) lsm.gyroData.y) / 4000.0;
+  float gyroZ = ((float) lsm.gyroData.z) / 4000.0;
+
+  float mag = sqrt(gyroY * gyroY + gyroZ * gyroZ);
+
+  if (mag >= 4.0) {
+    if (abs(gyroY) > abs(gyroZ)) {
+      // -- Mostly up-down
+      if (gyroY < 0.0) 
+        swing = UP;
+      else 
+        swing = DOWN;
+    } else {
+      // -- Mostly left-right
+      if (gyroZ < 0.0) 
+        swing = RIGHT;
+      else 
+        swing = LEFT;
+    }
+  }
+
+  return swing;
+}
+
+adel swing()
+{
+  int sound;
+  abegin:
+    if (g_saber_on) {
+      if (getSwing() != NONE) {
+        // -- Play a random swing
+        sound = SWING_MIN_SOUND + random(SWING_MAX_SOUND - SWING_MIN_SOUND);
+        myDFPlayer.play(sound);
+        // -- Wait for the sound to finish before starting a new one
+        // await( digitalRead(BUSY_BUTTON) == HIGH );
+        adelay(200);
+      } 
+    }
+  aend;
+}
+
+adel clash()
+{
+  int sound;
+  abegin:
+    if (g_saber_on) {
+      if (getClash()) {
+        // -- Play a random clash
+        sound = CLASH_MIN_SOUND + random(CLASH_MAX_SOUND - CLASH_MIN_SOUND);
+        myDFPlayer.play(sound);
+        // await( digitalRead(BUSY_BUTTON) == HIGH );
+        adelay(100);
+      }
+    }
+ 
+  aend;
+}
+
+// ----------------------------------------------------------------
+//  Saber on and off 
+//  Controls the scrolling effect
 
 adel saber_on()
 {
   abegin:
 
-  saber_start = millis();
-  while (millis() - saber_start < TIME_TO_ACTIVATE) {
-    curlevel = map(millis() - saber_start, 0, TIME_TO_ACTIVATE, 0, TOTAL_LENGTH);
-    render(curlevel);
+  aramp( TIME_TO_ACTIVATE, g_cur_length, 0, TOTAL_LENGTH ) {
+    render();
     adelay(10);
   }
 
-  render(TOTAL_LENGTH);
+  g_cur_length = TOTAL_LENGTH;
+  g_saber_on = true;
+  render();
 
   aend;
 }
@@ -109,82 +289,15 @@ adel saber_off()
 {
   abegin:
 
-  saber_start = millis();
-  while (millis() - saber_start < TIME_TO_DEACTIVATE) {
-    curlevel = map(millis() - saber_start, 0, TIME_TO_DEACTIVATE, TOTAL_LENGTH, 0);
-    render(curlevel);
+  aramp( TIME_TO_ACTIVATE, g_cur_length, TOTAL_LENGTH, 0 ) {
+    render();
     adelay(10);
   }
 
-  render(0);
+  g_cur_length = 0;
+  g_saber_on = false;
+  render();
 
-  aend;
-}
-
-bool get_swing(Direction& dir)
-{
-  lsm.read();
-
-  float gyroY = ((float) lsm.gyroData.y) / 4000.0;
-  float gyroZ = ((float) lsm.gyroData.z) / 4000.0;
-
-  dir = NONE;
-
-  float mag = sqrt(gyroY * gyroY + gyroZ * gyroZ);
-
-  if (mag < 3.0) {
-    dir = NONE;
-    return false;
-  } else {
-    if (abs(gyroY) > abs(gyroZ)) {
-      // -- Mostly up-down
-      if (gyroY < 0.0) 
-        dir = UP;
-      else 
-        dir = DOWN;
-    } else {
-      // -- Mostly left-right
-      if (gyroZ < 0.0) 
-        dir = RIGHT;
-      else 
-        dir = LEFT;
-    }
-
-    return true;
-  }
-}
-
-adel motion()
-{
-  avars {
-    int sound;
-    Direction dir;
-  }
-  
-  abegin:
-
-  while (1) {
-    if (g_saber_on) {
-      my(sound) = 0;
-      if (get_swing(my(dir))) {
-        if (my(dir) == UP)    my(sound) = SWING_1_SOUND;
-        if (my(dir) == DOWN)  my(sound) = SWING_1_SOUND;
-        if (my(dir) == LEFT)  my(sound) = SWING_3_SOUND;
-        if (my(dir) == RIGHT) my(sound) = SWING_2_SOUND;
-      }
-
-      if (my(sound) != 0) {
-        mp3_play(my(sound));
-        // -- Wait for the sound to finish before starting a new one
-        adelay(800);
-      }
-    }
-
-    // my(sound) = random(MEDIUM_HUM_SOUND, SHORT_HUM_SOUND+1);
-
-    adelay(10);
-  }
-  
   aend;
 }
 
@@ -194,27 +307,15 @@ adel onoff()
 
   while (1) {
     andthen( waitbutton(BUTTON_PIN) );
-    // andthen( trigger_sound(SABER_ON_SOUND) );
-    mp3_play(SABER_ON_SOUND);
+    myDFPlayer.play(SABER_ON_SOUND);
     adelay(200);
     andthen( saber_on() );
-    adelay(500);
-    mp3_play(LONG_HUM_SOUND);
-    g_saber_on = true;
     andthen( waitbutton(BUTTON_PIN) );
-    g_saber_on = false;
-    // andthen( trigger_sound(SABER_OFF_SOUND) );
-    mp3_play(SABER_OFF_SOUND);
+    myDFPlayer.play(SABER_OFF_SOUND);
+    adelay(500);
     andthen( saber_off() );
   }
   
-  aend;
-}
-
-adel saber()
-{
-  abegin:
-  atogether( onoff(), motion() );
   aend;
 }
 
@@ -229,28 +330,33 @@ void setup()
 
   // -- Set up the DFPlayer
   delay(200);
-  mp3_set_serial (Serial);  //set Serial for DFPlayer-mini mp3 module
-  delay(50);
-  // mp3_DAC(false);
-  // mp3_reset();
-  mp3_set_volume (30);
-  // delay(50);
-  // mp3_set_EQ (5);  //0~5
+  myDFPlayer.begin(Serial); // mp3_set_serial (Serial);  //set Serial for DFPlayer-mini mp3 module
+  myDFPlayer.volume(g_volume);  //Set volume value (0~30)
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  
   pinMode(BUTTON_PIN, INPUT);
   digitalWrite(BUTTON_PIN, LOW);
 
+  pinMode(BRIGHTNESS_A_PIN, INPUT);
+  digitalWrite(BRIGHTNESS_A_PIN, HIGH);                // Turn on internal pullup resistor
+  pinMode(BRIGHTNESS_B_PIN, INPUT);
+  digitalWrite(BRIGHTNESS_B_PIN, HIGH);                // Turn on internal pullup resistor
+
   // -- Set up the gyroscope
-  if (!lsm.begin())
-    Serial.println("Oops ... unable to initialize the LSM9DS0. Check your wiring!");
+  lsm.begin();
   lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
   lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
-
+  
   // -- Set up the PCA9685 PWM module  
   pwm.begin();
   pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
-  
-  render(0);
+
+  g_cur_length = 0;
+  render();
+
+  g_Mode = NO_MODE;
 }
 
 // -----------------------------------------------
@@ -259,6 +365,12 @@ void setup()
 
 void loop()
 {
-  arepeat( saber() );
+  lsm.read();
+  arepeat( onoff() );
+  aevery(20, clash() );
+  aevery(20, swing() );
+  arepeat( adjustments() );
+  arepeat( mode() );
 }
+
 
